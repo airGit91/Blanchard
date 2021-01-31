a94b22c0b53aee1305d2e9ee44c8daeaae9f8ca4
airGit91 <airgit1991@rambler.ru>
2021-01-31 12:38:24 +0300

    btn

--- scripts/index.js
+++ scripts/index.js
@@ -34,3 +34,18 @@ const element = document.querySelector('select');
                 searchEnabled: false,
                 itemSelectText: '',
             });
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    }else{
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
