8e05ba41e09a3c408d05569c14be43c1a8e7737e
Nik <air_nike9@icloud.com>
2021-02-19 15:14:38 +0300

    +

--- scripts/index.js
+++ scripts/index.js
@@ -109,23 +109,6 @@ window.addEventListener('DOMContentLoaded',function(){
       },
     });
 
-    // let myMap;
-    // // Дождёмся загрузки API и готовности DOM.
-    // ymaps.ready(init);
-
-    // function init () {
-    //     // Создание экземпляра карты и его привязка к контейнеру с
-    //     // заданным id ("map").
-    //     myMap = new ymaps.Map('map', {
-    //         // При инициализации карты обязательно нужно указать
-    //         // её центр и коэффициент масштабирования.
-    //         center: [55.76, 37.64], // Москва
-    //         zoom: 10
-    //     }, {
-    //         searchControlProvider: 'yandex#search'
-    //     });
-    // }
-
     ymaps.ready(function () {
       let myMap = new ymaps.Map('map', {
           center: [55.75784872240947,37.5994923144472],
@@ -153,4 +136,36 @@ window.addEventListener('DOMContentLoaded',function(){
   myMap.geoObjects
       .add(myPlacemarkWithContent);
   });
+
+  
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            let range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+        }
+    }
+    
+    function mask(event) {
+        let matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, "");
+        if (def.length >= val.length) val = def;
+        this.value = matrix.replace(/./g, function(a) {
+            return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        });
+        if (event.type == "blur") {
+            if (this.value.length == 2) this.value = ""
+        } else setCursorPosition(this.value.length, this)
+    };
+        let input = document.querySelector("#contacts__tel");
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+    
 });
