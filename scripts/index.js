ef79971b694cea7e02d5959c12f6412fa0f98168
Nik <airgit1991@rambler.ru>
2021-05-03 23:12:04 +0300

    +

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,14 @@ window.addEventListener('DOMContentLoaded',function(){
   $(document).ready(function () {
     $('.header__active').hide();
     $('.header__nav-btn').click(function(){
-      $(this).next().slideToggle('slow').siblings('div:visible').slideUp('slow');
-    });
-    // $(document).on('click', function(e) {
-    //   if (!$(e.target).closest(".header__nav-btn").length) {
-    //     $('.header__active').hide();
-    //   }
-    //   e.stopPropagation();
-    // });
-    $(document).mouseup(function (e){ // событие клика по веб-документу
-      var div = $(".header__active"); // тут указываем ID элемента
-      if (!div.is(e.target) // если клик был не по нашему блоку
-          && div.has(e.target).length === 0) { // и не по его дочерним элементам
-        div.hide(); // скрываем его
-      }
+      $(this).next().slideToggle('slow');
+      $(document).mouseup(function (e){ // событие клика по веб-документу
+        let div = $(".header__active"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+          div.hide(); // скрываем его
+        }
+      });
     });
   });
 
