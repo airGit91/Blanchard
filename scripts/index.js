148a8799712921623f370f3ab3bc2e3e620cb453
Nik <airgit1991@rambler.ru>
2021-07-01 21:31:08 +0300

    +

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-  $('.header__dropdown-active').hide();
+  $('.dropdown-active').hide();
+
   $('.header__button').click(function(e){
     let el = $(this).next();
-    // el.slideToggle(200);
-    $(this).toggleClass("header__dropdown-active");
+    el.slideToggle(200);
+    $(this).toggleClass("dropdown-active");
     $('.header__dropdown ').not(el).slideUp(200);
-    $('.header__button').not($(this)).removeClass('header__dropdown-active');
-    $(this).next().slideToggle('slow');
+    $('.header__button').not($(this)).removeClass('dropdown-active');
 
     $(this).css('backgroundImage', 'url(../icons/header-arr-up.svg)');
-
-    $(document).mouseup(function (e){ // событие клика по веб-документу
-      let div = $(".header__dropdown-active"); // тут указываем ID элемента
-      if (!div.is(e.target) // если клик был не по нашему блоку
-          && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
-        $('.header__dropdown-active').hide(); // скрываем его
-        $('.header__button').css('backgroundImage', 'url(../icons/header-arr-down.svg)');
-      }
-    });
-
   });
+
+      // $(document).mouseup(function (e){ // событие клика по веб-документу
+    //   let div = $(".dropdown-active"); // тут указываем ID элемента
+    //   if (!div.is(e.target) // если клик был не по нашему блоку
+    //       && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+    //     $('.dropdown-active').hide(); // скрываем его
+    //     $('.header__button').css('backgroundImage', 'url(../icons/header-arr-down.svg)');
+    //   }
+    // });
 
   $('.header__mob-menu').click(function() {
     $('body').toggleClass('slidemenu-open');
@@ -57,14 +56,9 @@ const element = document.querySelector('select');
 const choices = new Choices(element, {
     searchEnabled: false,
     itemSelectText: '',
-    // position: 'auto',
 });
 
 let galerySwiper = new Swiper(".galery__swiper", {
-  // observer: true,
-  // observeParents: true,
-  // cssMode: true,
-  loop: true,
   slidesPerView: 3,
   slidesPerColumn: 2,
   spaceBetween: 50,
